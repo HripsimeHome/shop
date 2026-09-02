e410a5a5e0e03a025609596f5cbab79371686ad1
Hripsime <m_hripsime@mail.ru>
2026-09-02 17:20:09 +0400

    Sass installation after  new building

---
+++ src/app/dashboard/orders/page.tsx
@@ -0,0 +1,7 @@
+export default function OrderPage() {
+  return (
+    <div>
+      <h2>Order</h2>
+    </div>
+  );
+}
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,7 +1,3 @@
-
-
 export default function Button() {
-  
-
-  return  ();
+  return <button>Click me</button>;
 }
